07c98c2097052515324cb35c9acaf0d54f73f2b1
Harsha <harshapu4@gmail.com>
2025-08-24 21:54:29 +0530

    Update Cards.jsx

--- src/components/Asthetics/Cards.jsx
+++ src/components/Asthetics/Cards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Cards({ title, description, imageUrl }) {
   return (
-    <div className="card bg-base-100 shadow-sm h-full flex flex-col rounded-3xl">
+    <div className="card bg-base-100 shadow-sm h-full flex flex-col rounded-3xl hover:scale-105 transition-transform">
       <figure>
         <img
           src={imageUrl}
@@ -10,10 +10,10 @@ function Cards({ title, description, imageUrl }) {
           className="w-full h-48 object-cover"
         />
       </figure>
-      <div className="card-body flex-grow flex flex-col justify-between bg-[#1c1629]">
-        <h2 className="card-title">{title}</h2>
-        <p>{description}</p>
-      </div>
+         <div className="card-body flex-grow flex flex-col justify-between bg-[#1c1629]">
+          <h2 className="card-title mt-1 text-xl font-bold  p-2">{title}</h2>
+          <p className='p-2 mb-2'>{description}</p>
+        </div>
     </div>
   )
 }
